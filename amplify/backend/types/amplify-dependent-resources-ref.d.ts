997bf14978e895f8ed5bef6e8964eddfe9f67875
SarthakD185 <ssd2115@rit.edu>
2025-01-29 11:56:19 -0500

    Revert "Major deployment changes"
    This reverts commit c26a94426b36af3c009960d5c16493a14fb24340.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "connect": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "anwUserConnect": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
