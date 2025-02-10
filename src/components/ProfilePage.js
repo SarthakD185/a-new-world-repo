05878bc4c18b1d3eac1b27fa0d87b12bcdc472ba
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-02-10 15:31:34 -0500

    Edited Profile Page to look like the wireframe

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import '../App.css';
-import '../assets/css/IndividualCollege.css';
+import '../assets/css/ProfilePage.css';
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
@@ -29,46 +29,101 @@ function ProfilePage() {
     
         <div>
 
+            {/* the header section */}
             <div class='horizontalFlex centerButton paddingTop'>
                 <img src={require(`../assets/images/${data.image}`)} class='smallLogo'></img>
                 <h1>{data.name}</h1>
             </div>
 
-            <div class='extraWideColumnContainer'>
+            <div class='pageGrid'>
                     
-                <div class='box' id='individualCollegeGallery'>
-                    <h2>not logged in...no pictures to see</h2>
-
-                    <GalleryList collegeID={data.id} />
-
-                </div>
-
-                <div class='box' id='individualCollegeRegisteredTeams'>
-                    <div class='horizontalFlex spaceBetween' id='registeredTeamsSearch'>
-                        <h2 class='noPadding noMargin'>Account Information</h2>
-
-                        {/* https://dev.to/salehmubashar/search-bar-in-react-js-545l */}
-                        <div className="search">
-                            <TextField
-                                id="outlined-basic"
-                                onChange={inputHandler}
-                                variant="outlined"
-                                fullWidth
-                                label="Search"
-                            />
-                        </div>
+   
+                    <div id='teamActionButtons'>
+                        <button class='heroButton'>Leave Team</button>
+                        <button class='heroButton'>Deactivate Account</button>
                     </div>
-                    
-                    <TeamList input={inputText} collegeID={data.id} />
 
+
+                <div class='box' id='accountInformation'>
+                    <div class='horizontalFlex spaceBetween'>
+                        <h2 class='noPadding noMargin'>Account Information</h2>
+                    </div>
+                    <div>
+                        <p>College Affiliation:</p>
+                        <p>Username:</p>
+                        <p>Full Name:</p>
+                        <p>Email Address</p>
+                        
+                    </div>
                 </div>
 
-                <div class='box' id='individualCollegeAnnouncements'>
+                <div class='box' id='registrationInformation'>
+                    <div class='horizontalFlex spaceBetween'>
+                        <h2 class='noPadding noMargin'>Registration Information</h2>
+                    </div>
+                    <div class='horizontalFlex spaceBetween'>
+                        <div>
+                            <p>Team Name:</p>
+                            <img src={"https://placehold.co/100"} class='smallLogo'></img>
+                        </div>
+
+                        <div>
+                            <p>Registration Status</p>
+                            <img src={"https://placehold.co/100"} class='smallLogo'></img>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div class='box' id='upcomingEvents'>
+                    <h2>Upcoming Events</h2>
+
+                    <h3>Opening Ceremony</h3>
+                    <p>Room: Auditorium A</p>
+                    <hr></hr>
+                    <h4>Match 1: Round of 32</h4>
+                    <p>YOUR TEAM Team 1[image] vs Team 2[image]</p>
+                    <p>Room: 123-A</p>
+                    <hr></hr>
+                    <h4>Game 2</h4>
+                    <p>Team 3[image] vs YOUR TEAM Team 1[image]</p>
+                    <p>Room: 456-B</p>
+                    <hr></hr>
+                    <h4>Game 3</h4>
+                    <p>YOUR TEAM Team 1[image] vs Team 4[image]</p>
+                    <p>Room: 2345-C</p>
+                    <hr></hr>
+                </div>
+
+
+                <div class='box' id='bioInformation'>
+                    <div class='horizontalFlex spaceBetween'>
+                        <h2 class='noPadding noMargin'>Your Bio</h2>
+                    </div>
+                    <div>
+                        <p>loren ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        
+                    </div>
+                </div>
+
+                <div class='box' id='teamGallery'>
+                    <div class='horizontalFlex spaceBetween'>
+                        <h2 class='noPadding noMargin'>Your Team's Gallery</h2>
+                    </div>
+                    <div>
+                        <GalleryList collegeID={data.id} />
+                    </div>
+                </div>
+               
+
+
+
+                {/* <div class='box' id='individualCollegeAnnouncements'>
                     <h2>Announcements</h2>
 
                     <AnnouncementsList collegeID={data.id} />
 
-                </div>
+                </div> */}
 
             </div>
 
