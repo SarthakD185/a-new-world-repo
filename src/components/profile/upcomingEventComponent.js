1580888cc4fb3f7609e39a76a59ed33a3fd09bfd
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-02-10 22:10:08 -0500

    Created reuseable event component. Updated Profile page and Profile css to take variable data and the new event component.

---
+++ src/components/profile/upcomingEventComponent.js
@@ -0,0 +1,40 @@
+import { React } from 'react';
+import '../../App.css';
+import '../../assets/css/ProfilePage.css';
+
+function UpcomingEventComponent({ 
+    eventTitle, 
+    team1Number, 
+    team2Number, 
+    location, 
+    team1Logo, 
+    team2Logo,
+    isYourTeam1 // boolean to indicate if "YOUR TEAM" label should be on team1 or team2
+}) {
+    return (
+        <>
+            <div class='horizontalFlex spaceBetween'>
+                <div>   
+                <h3>{eventTitle}</h3>
+                    <h3>
+                        {isYourTeam1 ? "YOUR TEAM " : ""}
+                        Team {team1Number}
+                        &nbsp;vs&nbsp;
+                        {!isYourTeam1 ? " YOUR TEAM " : ""}
+                        Team {team2Number}
+                    </h3>
+                    <p>Location: {location}</p>
+                </div>
+                <div class='horizontalFlex spaceBetween'>
+                    <img src={"https://placehold.co/100"} alt={`Team ${team1Number} logo`} className="smallLogo" />
+                    <img src={"https://placehold.co/100"} alt={`Team ${team2Number} logo`} className="smallLogo" />
+                </div>
+        
+            </div>
+            <hr></hr>
+        </>
+
+    )
+}
+
+export default UpcomingEventComponent;
