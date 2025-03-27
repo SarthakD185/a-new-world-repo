8f0b01798b625503bec18b0eb7e23670ab621d91
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-03-27 18:53:16 -0400

    Stripe button but is the real code

--- src/components/tournament/IndividualTournamentPage.js
+++ src/components/tournament/IndividualTournamentPage.js
@@ -131,9 +131,17 @@ function IndividualTournamentPage() {
                         <button className='standardButton largeButton' onClick={handleClick}>Sign Up Now!</button>
                     </div>
 
-                    <div className='centerButton'>
-                        <button className='standardButton largeButton'>Pay to Register - Goes to Stripe</button>
-                    </div>
+
+
+                    <script async
+                        src="https://js.stripe.com/v3/buy-button.js">
+                    </script>
+
+                    <stripe-buy-button
+                        buy-button-id="buy_btn_1R7Oy9RpKvOKttKIvGI6skqS"
+                        publishable-key="pk_test_51R7OhLRpKvOKttKI1UGW7zEzMiuGhpfAXtRxv9llNMmXY7M1Za9L6wp6PJrz5p518Z7yBhH2JON65DgcqK5ugWhE00gRD9y4us"
+                    >
+                    </stripe-buy-button>
 
                     {/* Conditionally render Create Tournament button for Moderators */}
                     {isAuthenticated && role === 'Moderator' && (
