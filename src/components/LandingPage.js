e833bb3ea63b954cb9349100321de131e24275df
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-04-06 21:38:37 -0400

    Modified mobile hero landing styles and modified popups for clickaway functionality

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -47,22 +47,21 @@ function LandingPage() {
 
                 <div id='landingPageSection2' className='box'>
                     <h2>Why Play?</h2>
-                    <p>
-                    All players who complete at least one round of tournament play will receive a <i><b>complimentary 
+                    <p> All players who complete at least one round of tournament play will receive a <i><b>complimentary 
                     copy of A New World.</b></i> Each university’s final round teams will go home with some awesome Aardvark 
                     Games swag. The First Place team for each university will receive a <i><b>cash prize of $1,000</b></i> and each 
-                    individual team member will get a <i><b>$100 gift certificate</b></i> for the Aardvark Games online store.
-                    </p>
-                    <p>
-                    Join us for some great gaming fun! Join us for some awesome tournament prizes!
-                    </p>
+                    individual team member will get a <i><b>$100 gift certificate</b></i> for the Aardvark Games online store.</p>
+                    <p> Join us for some great gaming fun! Join us for some awesome tournament prizes!</p>
+
+
                     <div className='centerButton'>
-                        <Popup trigger=
-                            {<button className='standardButton'>Frequently Asked Questions</button>} 
-                            modal nested>
+                        <Popup contentStyle={{ width: '90%' }} trigger=
+                            {<button className='centerButton standardButton' style={{ minWidth: '200px' }}>Frequently Asked Questions</button>} 
+                            // added clickaway functionality
+                            modal nested closeOnDocumentClick closeOnEscape>
                             {
                                 close => (
-                                    <div className='modal popup'>
+                                    <div className='popupContent' onClick={e => e.stopPropagation()}>
                                         <div className='content'>
 
                                             <h1 className='center'>Frequently Asked Questions</h1>
@@ -71,16 +70,16 @@ function LandingPage() {
                                             {/* TODO - add link */}
                                             <p>You can sign up for our pre-order list on the Game page. <i>[link]</i></p>
 
-                                            <h3 className='popupHeader'>I don’t see my university listed. Can I still play?</h3>
+                                            <h3 className='popupHeader'>I don't see my university listed. Can I still play?</h3>
                                             <p>Yes! When we see a new school listed, we contact them to initiate tournament activity. Please do sign up and get your friends involved ASAP!</p>
 
-                                            <h3 className='popupHeader'>Do I get my money back if my college doesn’t have playoff rounds?</h3>
+                                            <h3 className='popupHeader'>Do I get my money back if my college doesn't have playoff rounds?</h3>
                                             <p>Yes, those fees are refundable. However, if there are competitions played at nearby schools, you will first be invited to join a team there.</p>
 
-                                            <h3 className='popupHeader'>What if our team can’t afford to travel to the final playoff location?</h3>
+                                            <h3 className='popupHeader'>What if our team can't afford to travel to the final playoff location?</h3>
                                             <p>Aardvark games will be paying all travel costs.</p>
 
-                                            <h3 className='popupHeader'>I’m taking a break from college, can I play?</h3>
+                                            <h3 className='popupHeader'>I'm taking a break from college, can I play?</h3>
                                             <p>No, only current students may enter.</p>
 
                                         </div>
@@ -125,11 +124,12 @@ function LandingPage() {
 
                         <div className='centerButton'>
                             <Popup contentStyle={{ width: '90%' }} trigger=
-                                {<button className='standardButton'>View Player Roles</button>} 
-                                modal nested>
+                                {<button className='standardButton' style={{ minWidth: '200px' }}>View Player Roles</button>}
+                                // added clickaway functionality
+                                modal nested closeOnDocumentClick closeOnEscape>
                                 {
                                     close => (
-                                        <div className='popup'>
+                                        <div className='popupContent'>
                                             <div className='content'>
 
                                                 <h1 className='center'>Player Roles</h1>
@@ -137,22 +137,15 @@ function LandingPage() {
                                                 <p>Every team must designate the roles for each player prior to beginning play. If a team has fewer than seven players, team members may assume more than one role.</p>
 
                                                 <div className='twoColumnGrid'>
-
-                                                    <p><b>Expedition Leader: </b>This team member will make decisions on when and how action cards are played. They facilitate the team’s joint strategic planning and manage the expedition budget.</p>
-
-                                                    <p><b>Resource Specialist: </b>This team member is responsible for obtaining the resources required for survival on arrival and the establishment of a base on the new world.</p>
-
-                                                    <p><b>Scientist: </b>This team member collects knowledge cards that allow the team an advantage in knowing how to overcome obstacles and which actions are most likely to succeed.</p>
-
-                                                    <p><b>Technician: </b>This team member uses tool and technology cards to create the team base and repair machines and weapons as needed.</p>
-
-                                                    <p><b>Chronicler: </b>This team member is responsible for all communications with Home Base, for researching historic data cards that may aid the quest and for creating a chronicle of the current expedition.</p>
-
-                                                    <p><b>Weapons Specialist: </b>This team member leads the team defense strategies and works to gain points to raise each team member’s skill level on the weapon classes best suited to the current habitat.</p>
-                                                
+                                                    <p className='homeGridText'><b>Expedition Leader: </b>This team member will make decisions on when and how action cards are played. They facilitate the team's joint strategic planning and manage the expedition budget.</p>
+                                                    <p className='homeGridText'><b>Resource Specialist: </b>This team member is responsible for obtaining the resources required for survival on arrival and the establishment of a base on the new world.</p>
+                                                    <p className='homeGridText'><b>Scientist: </b>This team member collects knowledge cards that allow the team an advantage in knowing how to overcome obstacles and which actions are most likely to succeed.</p>
+                                                    <p className='homeGridText'><b>Technician: </b>This team member uses tool and technology cards to create the team base and repair machines and weapons as needed.</p>
+                                                    <p className='homeGridText'><b>Chronicler: </b>This team member is responsible for all communications with Home Base, for researching historic data cards that may aid the quest and for creating a chronicle of the current expedition.</p>
+                                                    <p className='homeGridText'><b>Weapons Specialist: </b>This team member leads the team defense strategies and works to gain points to raise each team member's skill level on the weapon classes best suited to the current habitat.</p>
                                                 </div>
 
-                                                <p><b>Physician: </b>This team member is responsible for the physical and mental health of expedition members, treating injuries and illness when determined by cards for encounters with native wildlife, hostile forces, space adaptation syndrome, etc.</p>
+                                                <p ><b>Physician: </b>This team member is responsible for the physical and mental health of expedition members, treating injuries and illness when determined by cards for encounters with native wildlife, hostile forces, space adaptation syndrome, etc.</p>
 
                                             </div>
                                             <div className='centerButton'>
