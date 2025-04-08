fe23aaf4aed0bf30336e87a244106c9e71e0dc58
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-04-08 02:40:09 -0400

    Check the signed in user email using isAuthenticated

--- src/components/tournament/StripeButton.jsx
+++ src/components/tournament/StripeButton.jsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { AccountContext } from "../../Account";
 
-import { useAuthenticator } from "@aws-amplify/ui-react";
+
 const StripeButton = () => {
-  const { user } = useAuthenticator((context) => [context.user]);
+  const { isAuthenticated, role, email, logout } = useContext(AccountContext); // Destructure the
+  
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/buy-button.js";
     script.async = true;
     document.body.appendChild(script);
 
+    console.log(email);
     return () => {
       document.body.removeChild(script);
     };
-  }, []);
+  }, [email]);
 
   return (
     <div id='stripeButton'>
       <stripe-buy-button
         buy-button-id="buy_btn_1R7Oy9RpKvOKttKIvGI6skqS"
-        client-reference-id={user.username}
+        client-reference-id={email}
+        customer-email={email}
         publishable-key="pk_test_51R7OhLRpKvOKttKI1UGW7zEzMiuGhpfAXtRxv9llNMmXY7M1Za9L6wp6PJrz5p518Z7yBhH2JON65DgcqK5ugWhE00gRD9y4us">
       </stripe-buy-button>
     </div>
--- src/components/tournament/TournamentPaymentPage.js
+++ src/components/tournament/TournamentPaymentPage.js
@@ -3,19 +3,23 @@ import '../../App.css';
 import '../../assets/css/IndividualTournament.css';
 import StripeButton from './StripeButton';
 import logo from '../../assets/images/ANewWorldTitleTextClearBackground.png';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function TournamentPaymentPage() {
     return (
-        <div>
-            <img src={logo} className='centerImagePadding' alt="logo"/>
-            <div className='centerButton' style={{flexDirection: 'column', gap: '20px'}}>
-                <h1>Tournament Registration Payment</h1>
-                <p>Complete your tournament registration by paying through Stripe</p>
-                <div style={{margin: '40px'}}>
-                    <StripeButton />
+        <Authenticator.Provider>
+            <div>
+                <img src={logo} className='centerImagePadding' alt="logo"/>
+                <div className='centerButton' style={{flexDirection: 'column', gap: '20px'}}>
+                    <h1>Tournament Registration Payment</h1>
+                    <p>Complete your tournament registration by paying through Stripe</p>
+                    <div style={{margin: '40px'}}>
+                        <StripeButton />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Authenticator.Provider>
     );
 }
 
