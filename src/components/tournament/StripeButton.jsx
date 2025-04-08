bff2c1a13ed1ebea740754a11ab4f1ce02fd8464
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2025-04-08 00:48:50 -0400

    added user auth...

--- src/components/tournament/StripeButton.jsx
+++ src/components/tournament/StripeButton.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 
+import { useAuthenticator } from "@aws-amplify/ui-react";
 const StripeButton = () => {
+  const { user } = useAuthenticator((context) => [context.user]);
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/buy-button.js";
@@ -16,6 +18,7 @@ const StripeButton = () => {
     <div id='stripeButton'>
       <stripe-buy-button
         buy-button-id="buy_btn_1R7Oy9RpKvOKttKIvGI6skqS"
+        client-reference-id={user.username}
         publishable-key="pk_test_51R7OhLRpKvOKttKI1UGW7zEzMiuGhpfAXtRxv9llNMmXY7M1Za9L6wp6PJrz5p518Z7yBhH2JON65DgcqK5ugWhE00gRD9y4us">
       </stripe-buy-button>
     </div>
